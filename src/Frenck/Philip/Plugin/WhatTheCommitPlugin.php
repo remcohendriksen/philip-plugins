981ef1404c8ac035a6766f53e164b749b53a2b24
Franck Nijhof <frenck@gmail.com>
2014-01-07 20:32:58 +0100

    Added first plugin: Random commit message generator (using WhatTheCommit.com)

---
+++ src/Frenck/Philip/Plugin/WhatTheCommitPlugin.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * This file is part of Frenck's Philip Plugins Package
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ *
+ * @copyright 2013 Franck Nijhof <frenck@gmail.com>
+ * @link      http://frenck.nl
+ */
+
+namespace Frenck\Philip\Plugin;
+
+use Philip\AbstractPlugin;
+use Philip\IRC\Response;
+use Philip\IRC\Event;
+use Guzzle\Http\Client;
+
+/**
+ * Gives a random commit message
+ *
+ * @author Franck Nijhof <frenck@gmail.com>
+ */
+class WhatTheCommitPlugin extends AbstractPlugin
+{
+    /**
+     * Initializing the plugin's behavior
+     *
+     * @return void
+     */
+    public function init()
+    {
+        $that = $this;
+
+        $this->bot->onChannel(
+            '/^!wtc$/',
+            function (Event $event) use ($that) {
+                $request = $event->getRequest();
+                $data = $that->getCommitMessage();
+                $event->addResponse(Response::msg($request->getSource(), $data));
+            }
+        );
+    }
+
+    /**
+     * Get a commit message from WhatTheCommit.com
+     *
+     * @return string the commit message
+     */
+    public function getCommitMessage()
+    {
+        $httpClient = new Client('http://whatthecommit.com');
+        $httpRequest = $httpClient->get('/index.txt');
+        $httpResponse = $httpRequest->send();
+
+        if ($httpResponse->isSuccessful() === true) {
+            return ucfirst($httpResponse->getBody(true));
+        }
+
+        return 'Sorry! I was unable to scrape a awesome commit message from the interwebs :(';
+    }
+
+    /**
+     * Returns plugin name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return 'WhatTheCommitPlugin';
+    }
+
+    /**
+     * Returns a help message for the plugin.
+     *
+     * @param Event $event The event
+     *
+     * @return string A simple help message.
+     */
+    public function displayHelp(Event $event)
+    {
+        return '!wtc - Gives a random commit message.';
+    }
+}
