7d8062d95f438616f5a7213f444e533bedd3822e
Remco Hendriksen <remco.hendriksen@webprint.nl>
2014-01-09 00:03:47 +0100

    Commit test

--- src/Frenck/Philip/Plugin/Base36Plugin.php
+++ src/Frenck/Philip/Plugin/Base36Plugin.php
@@ -3,7 +3,7 @@
  * This file is part of Frenck's Philip Plugins Package
  *
  * For the full copyright and license information, please view the LICENSE.txt
- * file that was distributed with this source code.
+ * file that was distributed with this source code. 
  *
  * @copyright 2014 Franck Nijhof <frenck@gmail.com>
  * @link      http://frenck.nl
